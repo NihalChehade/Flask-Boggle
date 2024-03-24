40a766f0c1fdb71d0ea9ae40a001d8d72136421e
Nihal Chehade <nihalche@icloud.com>
2024-03-23 23:24:21 -0700

    done

--- test_app.py
+++ test_app.py
@@ -44,19 +44,15 @@ class FlaskTests(TestCase):
 
     def test_end_Game(self):
         with app.test_client() as client:
-            with client.session_transaction() as change_session:
-                change_session['highscore'] = 8
+            # with client.session_transaction() as change_session:
+            #     change_session['highscore'] = 8
                 
-            res = client.post('/ending-game', data = {'score': 10})
-            # import pdb
-            # pdb.set_trace()
-
-              
-            # score = request.json['score']
+            res = client.post('/ending-game', json = {'score': 10})
+      
             self.assertEqual(res.status_code, 200)
             self.assertIn('highscore', session)
             self.assertEqual(session['num_played'], 1)
-            self.assertEqual(session['highscore'], 8)
+            self.assertEqual(session['highscore'],10)
 
             
             
